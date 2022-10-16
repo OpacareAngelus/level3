0fb4ca362da21e9c60e27e72166436b2ab8ad38a
OpacareAngelus <42981839+OpacareAngelus@users.noreply.github.com>
2022-10-16 23:26:25 +0300

    Update RecyclerAdapter.kt

--- app/src/main/java/adapter/RecyclerAdapter.kt
+++ app/src/main/java/adapter/RecyclerAdapter.kt
@@ -3,13 +3,11 @@ package adapter
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageButton
-import android.widget.ImageView
-import android.widget.TextView
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.example.level3.R
+import com.example.level3.databinding.RecyclerviewItemBinding
 import com.google.android.material.snackbar.Snackbar
 import extension.addImage
 import model.User
@@ -17,8 +15,7 @@ import model.UsersViewModel
 
 
 class RecyclerAdapter(
-    private val userList: UsersViewModel,
-//    private val onDeleteUser: (User) -> Unit
+    private val userList: UsersViewModel
 ) :
     ListAdapter<User, RecyclerAdapter.MyViewHolder>(object : DiffUtil.ItemCallback<User>() {
 
@@ -32,11 +29,22 @@ class RecyclerAdapter(
     }),
     View.OnClickListener {
 
-    class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        val nameField: TextView = itemView.findViewById(R.id.tv_name)
-        val careerField: TextView = itemView.findViewById(R.id.tv_career)
-        val userPhoto: ImageView = itemView.findViewById(R.id.iv_user_photo)
-        val deleteBtn: ImageButton = itemView.findViewById(R.id.img_btn_trash_can)
+    inner class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        lateinit var binding : RecyclerviewItemBinding
+
+        fun bind() {
+            binding = RecyclerviewItemBinding.bind(itemView)
+            binding.tvName.text = userList.getUser(adapterPosition)?.name
+            binding.tvCareer.text = userList.getUser(adapterPosition)?.career
+            userList.getUser(adapterPosition)?.let { binding.ivUserPhoto.addImage(it) }
+            binding.imgBtnTrashCan.tag = userList.getUser(adapterPosition)
+            userList.getUser(adapterPosition)?.id = adapterPosition
+
+            binding.imgBtnTrashCan.setOnClickListener {
+                val user = it?.tag as User
+                deleteUser(user, itemView)
+            }
+        }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
@@ -46,27 +54,16 @@ class RecyclerAdapter(
     }
 
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
-        holder.nameField.text = userList.getUser(position)?.name
-        holder.careerField.text = userList.getUser(position)?.career
-        userList.getUser(position)?.let { holder.userPhoto.addImage(it) }
-        userList.getUser(position)?.id = holder.adapterPosition
-        holder.deleteBtn.tag = userList.getUser(position)
-        holder.deleteBtn.setOnClickListener(this)
+        holder.bind()
     }
 
     override fun getItemCount(): Int = userList.size()!!
 
-    override fun onClick(v: View?) {
-        val user = v?.tag as User
-        when (v.id) {
-            R.id.img_btn_trash_can -> {
-                deleteUser(user, v)
-            }
-        }
+    override fun onClick(view: View?) {
     }
 
-    private fun deleteUser(user: User, v: View) {
-        val delMessage = Snackbar.make(v, "${user.name} has deleted.", Snackbar.LENGTH_LONG)
+    private fun deleteUser(user: User, view: View) {
+        val delMessage = Snackbar.make(view, "${user.name} has deleted.", Snackbar.LENGTH_LONG)
         userList.delete(user.id)
         notifyItemRemoved(user.id)
         userList.size()?.let { notifyItemRangeChanged(user.id, it) }
@@ -76,10 +73,10 @@ class RecyclerAdapter(
 
     /**Method back to list of contacts deleted contact if user push "Cancel" on the Snackbar.*/
     private fun undoUserDeletion(user: User, delMessage: Snackbar) {
-        delMessage.setAction("Cancel", View.OnClickListener() {
+        delMessage.setAction("Cancel") {
             userList.add(user)
             userList.size()?.let { it1 -> notifyItemRangeChanged(user.id, it1) }
-        })
+        }
     }
 }
 
