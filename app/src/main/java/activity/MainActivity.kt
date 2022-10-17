497a0ae459cc8f628ebbc5f108843a524c7afce1
OpacareAngelus <42981839+OpacareAngelus@users.noreply.github.com>
2022-10-17 20:52:19 +0300

    Update MainActivity.kt

--- app/src/main/java/activity/MainActivity.kt
+++ app/src/main/java/activity/MainActivity.kt
@@ -3,11 +3,8 @@ package activity
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.level3.R
-import model.UsersViewModel
 
 class MainActivity : AppCompatActivity() {
-
-    val userList : UsersViewModel = UsersViewModel()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
