2bca56d4692003510c9bf7b89a932d02c4572cc5
OpacareAngelus <42981839+OpacareAngelus@users.noreply.github.com>
2022-10-23 20:26:35 +0300

    SimpleCallBack fix
    -Was add interface
    -Re-new capitalized method(adapter possition to absoluteAdapterPosition

--- app/src/main/java/util/SimpleCallBack.kt
+++ app/src/main/java/util/SimpleCallBack.kt
@@ -1,4 +1,4 @@
-import adapter.RecyclerAdapterUserContacts
+
 import androidx.core.os.bundleOf
 import androidx.navigation.NavController
 import androidx.recyclerview.widget.ItemTouchHelper
@@ -7,14 +7,13 @@ import com.example.level3.R
 import com.google.android.material.snackbar.Snackbar
 import model.User
 import model.UsersViewModel
+import util.UserListController
 
 class SimpleCallBack(
-    var viewModel: UsersViewModel,
-    var usersAdapter: RecyclerAdapterUserContacts,
-    var navGraph: NavController
+    val viewModel: UsersViewModel,
+    private val navGraph: NavController,
+    val userListController: UserListController
 ) {
-
-    //Swipe here
 
     var simpleCallback =
         object : ItemTouchHelper.SimpleCallback(
@@ -25,24 +24,19 @@ class SimpleCallBack(
                 viewHolder: RecyclerView.ViewHolder,
                 target: RecyclerView.ViewHolder
             ): Boolean {
-                TODO("Not yet implemented")
+                return true
             }
 
             override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
                 when (direction) {
                     ItemTouchHelper.LEFT -> {
-                        val user = viewModel.getUser(viewHolder.adapterPosition)
-                        val delMessage = Snackbar.make(
-                            viewHolder.itemView,
+                        val user = viewModel.getUser(viewHolder.absoluteAdapterPosition)
+                        val delMessage = Snackbar.make(viewHolder.itemView,
                             "${user?.name} has deleted.",
                             Snackbar.LENGTH_LONG
                         )
-                        usersAdapter.submitList(viewModel.userListLiveData.value.also {
-                            viewModel.userListLiveData.value?.removeAt(
-                                viewHolder.adapterPosition
-                            )
-                        }?.toMutableList())
-                        user?.let { undoUserDeletion(it, delMessage, viewHolder.adapterPosition) }
+                        userListController.onDeleteUser(viewHolder.absoluteAdapterPosition)
+                        user?.let { undoUserDeletion(it, delMessage) }
                         delMessage.show()
                     }
                     ItemTouchHelper.RIGHT -> {
@@ -53,13 +47,9 @@ class SimpleCallBack(
         }
 
     /**Method back to list of contacts deleted contact if user push "Cancel" on the Snackbar.*/
-    private fun undoUserDeletion(user: User, delMessage: Snackbar, position: Int) {
+    private fun undoUserDeletion(user: User, delMessage: Snackbar) {
         delMessage.setAction(R.string.cancel) {
-            usersAdapter.submitList(viewModel.userListLiveData.value.also {
-                viewModel.userListLiveData.value?.add(
-                    user!!
-                )
-            }?.toMutableList())
+            userListController.onContactAdd(user)
         }
     }
 
@@ -67,10 +57,10 @@ class SimpleCallBack(
         navGraph.navigate(
             R.id.action_fragmentContacts_to_fragmentContactProfile,
             bundleOf(
-                Pair("photo", viewModel.getUser(viewHolder.adapterPosition)?.photo),
-                Pair("name", viewModel.getUser(viewHolder.adapterPosition)?.name),
-                Pair("career", viewModel.getUser(viewHolder.adapterPosition)?.career),
-                Pair("address", viewModel.getUser(viewHolder.adapterPosition)?.homeAddress)
+                Pair("photo", viewModel.getUser(viewHolder.absoluteAdapterPosition)?.photo),
+                Pair("name", viewModel.getUser(viewHolder.absoluteAdapterPosition)?.name),
+                Pair("career", viewModel.getUser(viewHolder.absoluteAdapterPosition)?.career),
+                Pair("address", viewModel.getUser(viewHolder.absoluteAdapterPosition)?.homeAddress)
             )
         )
     }
