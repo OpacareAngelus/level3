f48b79a3ecfb67eeb1c519fc2c7c270c813ec4fe
OpacareAngelus <42981839+OpacareAngelus@users.noreply.github.com>
2022-10-23 20:31:52 +0300

    Update: FragmentContactProfile
    -Little decomposition of onCreateView
    -Code Style fixes.
    -Delete overrided ImageView.addImage. Now this method in the extensions.

--- app/src/main/java/fragments/FragmentContactProfile.kt
+++ app/src/main/java/fragments/FragmentContactProfile.kt
@@ -4,40 +4,41 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
-import com.bumptech.glide.Glide
-import com.example.level3.databinding.ContactProfileBinding
+import com.example.level3.databinding.FragmentContactProfileBinding
+import extension.addImage
 
 class FragmentContactProfile : Fragment() {
 
-    private lateinit var binding: ContactProfileBinding
+    private lateinit var binding: FragmentContactProfileBinding
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        val bundle = arguments
-        addData(bundle)
-        binding.imgBtnBackArrow.setOnClickListener{
-            findNavController().navigateUp()
-        }
+    ): View {
         return binding.root
     }
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        if (arguments != null) {
+            val bundle = arguments
+            addData(bundle)
+        }
+        binding.imgBtnBackArrow.setOnClickListener {
+            findNavController().navigateUp()
+        }
+    }
     private fun addData(bundle: Bundle?) {
-        binding = ContactProfileBinding.inflate(layoutInflater)
-        binding.ivUserPhoto.addImage(bundle?.let { FragmentContactsArgs.fromBundle(it).photo })
-        binding.tvName.text = bundle?.let { FragmentContactsArgs.fromBundle(it).name }
-        binding.tvCareer.text = bundle?.let { FragmentContactsArgs.fromBundle(it).career }
-        binding.tvUserAddress.text = bundle?.let { FragmentContactsArgs.fromBundle(it).address }
+        binding = FragmentContactProfileBinding.inflate(layoutInflater)
+        binding.apply {
+            ivUserPhoto.addImage(bundle?.let { FragmentContactsArgs.fromBundle(it).photo })
+            tvName.text = bundle?.let { FragmentContactsArgs.fromBundle(it).name }
+            tvCareer.text = bundle?.let { FragmentContactsArgs.fromBundle(it).career }
+            tvUserAddress.text = bundle?.let { FragmentContactsArgs.fromBundle(it).address }
+        }
     }
 }
 
-private fun ImageView.addImage(photo: String?) {
-    Glide.with(this.context)
-        .load(photo)
-        .into(this)
-}
